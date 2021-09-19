67a08565554c79e897bbe922c9c122eecc1f808b
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-09-18 18:48:58 -0700

    building out processor that accesses the API for recipes

---
+++ APIClientManager/EdamamRecipeAPI/RecipeProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.dll;
+
+namespace APIClientManager.EdamamRecipeAPI
+{
+    public class RecipeProcessor : ProcessorInterface
+    {
+        public async Task GetByIdAsync(int Id = 0)
+        {
+            URI websiteIdentifier;
+
+            if(Id > 0)
+            {
+                websiteIdentifier = new URI($"https://api.edamam.com/api/recipes/v2/{ Id }");
+            }
+            else
+            {
+                websiteIdentifier = null;
+            }
+
+            // prevent requests if URI is invalid
+            if (websiteIdentifier != null)
+            {
+                // generate a response from the API given the url 
+                // the url will be used to make a URI (Uniform resource identifier)
+                using (HttpResponseMessage response = await APIHelper.APIClient.SendAsync(websiteIdentifier))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ APIClientManager/ProcessorInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIClientManager
+{
+    interface ProcessorInterface
+    {
+        public Task GetByIdAsync(int Id = 0);
+
+
+    }
+}
